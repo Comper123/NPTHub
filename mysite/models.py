5ab313a45efe715ce391ce21497c98f18b28e58f
Comper <ibarishnikov07@mail.ru>
2024-10-26 22:58:06 +0300

    Добавил модель профиля пользователя

--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils.timezone import localdate
 
 
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+class Profile(models.Model):
+    """Класс профиля пользователя"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", default=None, null=True)
+    name = models.CharField("Имя", max_length=100, default="User")
+    telegram = models.CharField("Телеграм", max_length=50, default="")
+    register_date = models.DateField(default=localdate)
+    photo = models.ImageField("Фото профиля", null=True, blank=True, upload_to='my_site/static/img/usersphotos/', 
+                              default='my_site/static/img/usersphotos/default.png')
+    age = models.IntegerField("Возраст", default=18)
+    organization = models.CharField("Организация", max_length=100,
+                                     default="Самозанятый")
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'профиль'
+        verbose_name_plural = 'Профили'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
